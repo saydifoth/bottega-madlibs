b55f5b429f51d2e771f547eb99f217d15b815e51
saydifoth <saydimccall@gmail.com>
2018-07-05 10:09:46 -0600

    changed port and app title

---
+++ app.js
@@ -0,0 +1,12 @@
+import Recat, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Bottega Madlibs</h1>
+            </div>
+
+        );
+    }
+}
